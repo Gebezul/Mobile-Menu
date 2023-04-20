b2cedf6ceddded87e7d38dd96a26161ed3217653
Omar <omgebeev@mail.ru>
2023-04-20 14:27:14 +0400

    Uploaded Final Release

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-document.querySelector('.hamburger').addEventListener('click', function(event) {
-    event.preventDefault();
-    if (this.classList.contains('is-active')) {
-        this.classList.remove('is-active');
-        document.querySelector('#menu').classList.remove('nav-active');
-        document.body.classList.remove('body-active');
-    } else {
-        this.classList.add('is-active');
-        document.querySelector('#menu').classList.add('nav-active');
-        document.body.classList.add('body-active');
-    }
+const hambMenu = document.querySelector('.hamburger')
+hambMenu.addEventListener('click', function() {
+     
+    this.classList.toggle('is-active')
+
+    document.querySelector('#menu').classList.toggle('nav-active');
+
+    document.body.classList.toggle('body-active');
+
 })
